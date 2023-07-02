a374726d9b2dfa3285bbd48de0197f002dd6db3d
Adenike23 <adeofeadenike@gmail.com>
2023-07-02 17:03:00 +0100

    carousel functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 AOS.init();
 
-const openNav = document.querySelector(".open-menu");
+    const openNav = document.querySelector(".open-menu");
     const closeNav = document.querySelector(".close-menu");
     const navList = document.querySelector("nav ul");
-    openNav.addEventListener("click", function() {
+openNav.addEventListener("click", function() {
     navList.style.top = "5%"
     openNav.style.display = "none"
     closeNav.style.display = "block"
@@ -20,3 +20,32 @@ document.querySelectorAll("nav ul a").forEach(navLink => {
         closeNav.style.display = "none"
     })
 })
+
+$(".testimonials").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    margin: 50,
+    dots: true,
+    arrows: true,
+    nav: false,
+    // navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+        loop: true,
+      },
+      600: {
+        items: 2,
+        nav: true,
+        loop: true,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+  
